d93b01c10612e5c4c7edd08910dfe2f3dabcf05b
hadihasan953 <22030168@students.liu.edu.lb>
2025-09-25 01:30:59 +0400

    fix: correct import statements and update health check route structure

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
@@ -7,7 +7,7 @@ dotenv.config();
 const app: Express = express();
 
 const allowedOrigins = [
-    'http://localhost::5500'
+    'http://localhost:5500'
 ]
 
 app.use(cors({
@@ -20,8 +20,28 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 const prefix = process.env.PREFIX || 'api/';
-const version = process.env.VERSION || 'v1/';
+const version = process.env.VERSION || 'v1';
+
+const baseUrl = `/${prefix}${version}`;
+
+// Health Check - used to check if the server is running
+app.get(`${baseUrl}/health`, (_req: Request, res: Response) => {
+    res.json({
+        status: 'success',
+        statusCode: 200,
+        message: 'Server is running fine',
+    });
+});
+
+// 404 - Must be be below all other routes
+app.use((_req: Request, res: Response) => {
+    res.status(404).json({
+        status: 'fail',
+        statusCode: 404,
+        message: 'Route not found',
+    });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on: http://localhost:${port}/${prefix}${version}`); // http://localhost:3000/api/v1/
+    console.log(`Server is running on: http://localhost:${port}${baseUrl}`); // http://localhost:3000/api/v1/
 });
