4aaf560117c1d6a286fb0b5abaf6ef88c8fdd9a3
hadihasan953 <22030168@students.liu.edu.lb>
2025-09-25 13:58:50 +0400

    refactor: update sequelize configuration to use environment variables for database connection

--- src/utils/sequelize.ts
+++ src/utils/sequelize.ts
@@ -1,7 +1,16 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize('database_name', 'root', '', {
-    host: 'localhost',
+dotenv.config();
+
+const dbName = process.env.DB_NAME as string;
+const dbUser = process.env.DB_USER as string;
+const dbPass = process.env.DB_PASS as string;
+const dbHost = process.env.DB_HOST as string;
+const dbDialect = process.env.DB_DIALECT as string;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPass, {
+    host: dbHost,
     dialect: 'mysql',
 });
 
