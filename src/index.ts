39e2c785814a51b52bafa5e6f3fc2203c29d07db
hadihasan953 <22030168@students.liu.edu.lb>
2025-09-29 23:58:59 +0400

    feat: add additional allowed origin for CORS configuration

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,8 @@ dotenv.config();
 const app: Express = express();
 
 const allowedOrigins = [
-    'http://localhost:5500'
+    'http://localhost:5500',
+    'http://localhost:5173'
 ];
 
 app.use(cors({
